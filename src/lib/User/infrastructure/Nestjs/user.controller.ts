d83c16317cfaa6aa565e5484f2478a62db8a61fb
Israel <israel.pe@outlook.es>
2024-08-07 11:10:37 -0500

    feat: added endpoint create user with validation

--- src/lib/User/infrastructure/Nestjs/user.controller.ts
+++ src/lib/User/infrastructure/Nestjs/user.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get, HttpException, Inject, Query } from '@nestjs/common';
-import { UserGetAll, UserGetOneById } from '../../application';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  Inject,
+  InternalServerErrorException,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserCreate, UserGetAll, UserGetOneById } from '../../application';
 import { CustomError } from '../../domain/errors/custom.error';
+import { Create } from './dtos/create.dto';
 
 @Controller('user')
 export class UserController {
   constructor(
     @Inject('UserGetAll') private readonly userGetAll: UserGetAll,
     @Inject('UserGetOneById') private readonly userGetOneById: UserGetOneById,
+    @Inject('UserCreate') private readonly userCreate: UserCreate,
   ) {}
 
   @Get()
@@ -27,10 +40,27 @@ export class UserController {
     }
   }
 
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createUser(@Body() user: Create) {
+    try {
+      return await this.userCreate.run(
+        user.id,
+        user.name,
+        user.email,
+        new Date(),
+      );
+    } catch (e) {
+      this.handleError(e);
+    }
+  }
+
   private handleError = (error: unknown) => {
+    console.log(error); //Wiston
+
     if (error instanceof CustomError)
       throw new HttpException(error.message, error.statusCode);
-    console.log(error); //Wiston
-    return error;
+
+    throw new InternalServerErrorException(error);
   };
 }
