030f62c080f9a519c9eb402a664aaf3ab48483f3
Israel <israel.pe@outlook.es>
2024-08-07 17:04:28 -0500

    feat: controller test getall

--- src/lib/User/infrastructure/Nestjs/user.controller.spec.ts
+++ src/lib/User/infrastructure/Nestjs/user.controller.spec.ts
@@ -1,12 +1,71 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import spyOn = jest.spyOn;
+import { User } from '../../domain/User';
+import { UserId } from '../../domain/UserId';
+import { UserName } from '../../domain/UserName';
+import { UserEmail } from '../../domain/UserEmail';
+
+const mockedUsers = [
+  {
+    id: 'test_user_1',
+    name: 'Israel Perez',
+    email: 'israel.pe@outlook.es',
+    createdAt: '2024-08-07T20:24:18.134Z',
+    updatedAt: '2024-08-07T20:31:25.993Z',
+  },
+  {
+    id: 'test_user_2',
+    name: 'Jose Espinosa',
+    email: 'jose.es@outlook.es',
+    createdAt: '2024-08-07T20:31:48.166Z',
+    updatedAt: undefined,
+  },
+];
 
 describe('UserController', () => {
   let controller: UserController;
-
+  const mockUserGetAll = {
+    run: jest.fn(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: 'UserGetAll',
+          useValue: mockUserGetAll,
+        },
+        {
+          provide: 'UserGetOneById',
+          useFactory: (id: string) => {
+            run: jest.fn(() => mockedUsers[0]);
+          },
+        },
+        {
+          provide: 'UserCreate',
+          useFactory: () => {
+            run: jest.fn(
+              (id: string, name: string, email: string, createdAt: Date) =>
+                Promise<void>,
+            );
+          },
+        },
+        {
+          provide: 'UserEdit',
+          useFactory: () => {
+            run: jest.fn(
+              (id: string, name?: string, email?: string) => Promise<void>,
+            );
+          },
+        },
+        {
+          provide: 'UserDelete',
+          useFactory: () => {
+            run: jest.fn((id: string) => Promise<void>);
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
@@ -14,5 +73,20 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call method "run" of userGetAll usecase', async () => {
+    const user = new User({
+      id: new UserId(mockedUsers[1].id),
+      name: new UserName(mockedUsers[1].name),
+      email: new UserEmail(mockedUsers[1].email),
+      createdAt: new Date(mockedUsers[1].createdAt),
+    });
+    const users = [user];
+    jest.spyOn(mockUserGetAll, 'run').mockReturnValue(users);
+
+    const res = await controller.getAll();
+    expect(res).toEqual([mockedUsers[1]]);
+    expect(mockUserGetAll.run).toHaveBeenCalled();
   });
 });
